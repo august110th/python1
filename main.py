5bd7e13c8074852a225d8294aec81835fff3f4de
august110th <august110th@ya.ru>
2021-05-20 23:52:30 +0400

    done

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
 from application import salary
 from application.db import people
+from datetime import datetime
+
+def date_now():
+    current_datetime = datetime.now()
+    print(current_datetime)
+
 
 if __name__ == '__main__':
     salary.calculate_salary()
     people.get_employees()
+    date_now()
