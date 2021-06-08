07dbd04fb0b4d1e3dfaa377be10877e784840d54
august110th <august110th@ya.ru>
2021-06-08 22:51:36 +0400

    done

--- main.py
+++ main.py
@@ -2,11 +2,24 @@ from application import salary
 from application.db import people
 from datetime import date
 
-def date_now():
-    current_date = date.today()
-    print(current_date)
+# def date_now():
+#     current_date = date.today()
+#     print(current_date)
+
+def date_now_logger(function):
+    def logger():
+        print('decor')
+        result = function()
+        return result
+    return logger
+
+
 
 if __name__ == '__main__':
     salary.calculate_salary()
     people.get_employees()
-    date_now()
+    # date_now()
+@date_now_logger
+def date_now():
+    current_date = date.today()
+    print(current_date)
