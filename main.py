8c45b5a85bf23c56dd5410bc4326b65cdcba59e2
august110th <august110th@ya.ru>
2021-06-10 20:30:33 +0400

    done

--- main.py
+++ main.py
@@ -5,27 +5,20 @@ from application.db import people
 from datetime import datetime, date, time
 from datetime import time
 
-# def date_now():
-#     current_date = date.today()
-#     print(current_date)
-
 def date_now_logger(function):
-    def logger(*args, **kwargs):
-        print('decor')
-        function()
-        result1 = date_now_logger(function)
+    def logger(**kwargs):
+        result1 = function()
         with open('C:\logs_python.txt', 'a', encoding='utf-8') as f:
-            f.write(f'{datetime.now}, {function.__name__}, {result1} \n')
+            f.write(f'функция запущена: {datetime.now()}, название функции: {function.__name__}, параметры: {kwargs}, результат: {result1} \n')
         return result1
     return logger
 
 if __name__ == '__main__':
     salary.calculate_salary()
     people.get_employees()
-    # date_now()
-# @date_now_logger(r'C:\logs_python.txt')
-@date_now_logger
+     # date_now()
+@date_now_logger(r'C:\logs_python.txt')
 def date_now():
     result = date.today()
-    return print(result)
-date_now()
+    return result
+date_now(x=1)
