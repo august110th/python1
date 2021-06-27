07bafe67c58f58794af8c70c950128447a1d60d1
august110th <august110th@ya.ru>
2021-06-27 16:44:28 +0400

    done

--- main.py
+++ main.py
@@ -2,20 +2,24 @@ from application import salary
 from application.db import people
 from datetime import datetime, date
 
-def decorator(file, *args):
-     def logger(old_function):
-         result_old = old_function(*args)
-         with open(file, 'a', encoding='utf-8') as f:
-             f.write(f'функция запущена: {datetime.now()}, название функции: {old_function.__name__}, аргументы:{args}, результат: {result_old} \n')
-         return old_function
-     return logger
+def decorator(file):
+    def logger(old_function):
+        def new_function(*args, **kwargs):
+            result = old_function(*args, **kwargs)
+            with open(file, 'a', encoding='utf-8') as f:
+                f.write(f'функция запущена: {datetime.now()}, название функции: {old_function.__name__}, аргументы:{args, kwargs}, результат: {result} \n')
+            return result
+        return new_function
+    return logger
 
 if __name__ == '__main__':
     salary.calculate_salary()
     people.get_employees()
     # date_now()
-@decorator(r'C:\logs_python.txt', 1, 2)
-def date_now(*args):
+
+@decorator(r'C:\Users\august110th\Documents\logs_python.txt')
+def date_now(*args, **kwargs):
     result = date.today()
     return result
-date_now()
+
+date_now(1,2, b='for')
